282c1d94327c3806a917314516617ec89c01c783
info@totalrevolution.tv <info@totalrevolution.tv>
2017-05-03 19:31:37 +0100

    Renamed test repo back to old spartacus one

--- zips/repository.trmodules/service.py
+++
@@ -1,6 +0,0 @@
-import xbmc
-if not xbmc.getCondVisibility('System.HasAddon(script.openwindow)'):
-    try:
-        xbmc.executebuiltin('InstallAddon(script.openwindow)')
-    except:
-        pass
